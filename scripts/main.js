ea04648f23fbca6f2631b0bc574cfcd0865949fb
John Lins <johnpaullins@gmail.com>
2021-03-02 09:44:22 -0800

    update

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,11 @@ var URL = "";
 var APIURL = "http://localhost:8080"//"http://localhost:8080";
 //https://ancient-thicket-11529.herokuapp.com
 
+function gauge(id, num){
+    document.getElementById(id).querySelector(".gauge").style= "opacity: 0." + (num.toString() + 10) + ";"
+    document.getElementById(id).querySelector(".gauge .fill").style= "width: " + num.toString() + "%;"
+}
+
 
 if(sessionStorage.getItem("landingURLInput") != ""){
     URL = sessionStorage.getItem("landingURLInput");
@@ -72,6 +77,16 @@ function stats(){
         "<b>ViewPort: </b>" + response.FetchMeta.viewport + "<br/>" +
         "<b>Robots: </b>" + response.FetchMeta.robots
 
+        
+        if(response.FetchMeta.description != null){
+            window.alert("test")
+            gauge(`current_meta_out`, 50)
+            if(response.FetchMeta.keywords != null){
+                gauge(`current_meta_out`, 98)
+            }
+        }
+        
+
        
         
         //document.querySelector("#validator_out").querySelector("span").innerText = response.Validator
@@ -88,10 +103,28 @@ function stats(){
 
 document.querySelector("#current_og_meta_out").querySelector(".output").innerHTML = "<b>Description: </b>" + response.FetchMetaOG.description + "<br/><b>Keywords: </b>" + response.FetchMetaOG.keywords + "<br/><b>Title: </b>" + response.FetchMetaOG.title + `<br/><b>OG Meta Image: </b> <img src=\"` + response.FetchMetaOG.image + `\"` + `/>`;
 
+if(response.FetchMetaOG.description != null){
+    window.alert("test")
+    gauge(`current_og_meta_out`, 10)
+    if(response.FetchMetaOG.keywords != null){
+        gauge(`current_og_meta_out`, 20)
+        if(response.FetchMetaOG.title != null){
+            gauge(`current_og_meta_out`, 50)
+            if(response.FetchMetaOG.image != null){
+                gauge(`current_og_meta_out`, 98)
+            }
+        }
+    }
+}
+
         
         /*document.querySelector("#import_sizes_out").querySelector("span").innerText = response.FetchImportSizes.CSSSize
         document.querySelector("#import_sizes_out").querySelector("span").innerText = response.FetchImportSizes.IFRAMESize*/
         document.querySelector("#import_sizes_out").querySelector(".output").querySelector("span").innerHTML = "<b>Image Size: </b>" + response.FetchImportSizes.IMAGESize + " KB <br/>" + "<b>CSS Size: </b>" + response.FetchImportSizes.CSSSize + " KB <br/>" + "<b>JavaScript Size </b>" + response.FetchImportSizes.JSSize + " KB <br/>" + "<b>Iframe Size </b>" + response.FetchImportSizes.IFRAMESize + "KB"
+
+        totalImportSize = Number(response.FetchImportSizes.IMAGESize) + Number(response.FetchImportSizes.CSSSize) + Number(response.FetchImportSizes.JSSize) + Number(response.FetchImportSizes.IFRAMESize)
+        window.alert(totalImportSize)
+        gauge(`import_sizes_out`, ceil(totalImportSize / 4))
         /*document.querySelector("#import_sizes_out").querySelector("span").innerText = response.FetchImportSizes.JSSize*/
 
         document.querySelector("#request_speed_out").querySelector(".output").querySelector("span").innerText = response.RequestSpeed + " Nanoseconds?"
@@ -239,3 +272,13 @@ function renderValidatorToHtml(id) {
   }
 
   */
+
+
+
+ function copyToClipboard(id) {
+    var copyText = document.getElementById(id).querySelector(".output").querySelector("textarea")
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); 
+    document.execCommand("copy");
+    alert("Copied!: " + copyText.value);
+}
