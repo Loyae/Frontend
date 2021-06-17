290013c8c8e1d93505d3260b59ed6f5f97be5c6d
John Lins <johnpaullins@gmail.com>
2021-06-17 14:13:49 -0700

    image

--- scripts/main.js
+++ scripts/main.js
@@ -115,7 +115,8 @@ function stats(){
             
         //}
 
-document.querySelector("#current_og_meta_out").querySelector(".output").innerHTML = "<b>Description: </b>" + response.FetchMetaOG.description + "<br/><b>Keywords: </b>" + response.FetchMetaOG.keywords + "<br/><b>Title: </b>" + response.FetchMetaOG.title + `<br/><b>OG Meta Image: </b> <br/><img src=\"` + response.FetchMetaOG.image + `\"` + ` style="height:150;"/>`;
+        var OGimage = (response.FetchMetaOG.image != "") ?  `<br/><b>OG Meta Image: </b> <br/><img src=\"` + response.FetchMetaOG.image + `\"` + ` style="height:150;"/>` : "";
+document.querySelector("#current_og_meta_out").querySelector(".output").innerHTML = "<b>Description: </b>" + response.FetchMetaOG.description + "<br/><b>Keywords: </b>" + response.FetchMetaOG.keywords + "<br/><b>Title: </b>" + response.FetchMetaOG.title + OGimage;
 
 if(response.FetchMetaOG.description != null){
     //window.alert("test")
